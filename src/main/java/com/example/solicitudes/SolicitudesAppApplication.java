078b8190a509be40210764b062b58281225857e2
Adrian Omar Palacios Medrano <adrian.palacios@tecsup.edu.pe>
2019-06-12 12:34:58 -0500

    primer commit

---
+++ src/main/java/com/example/solicitudes/SolicitudesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.solicitudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudesAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/solicitudes/services/SolicitudService.java
@@ -0,0 +1,23 @@
+package com.example.solicitudes.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.solicitudes.models.Solicitud;
+import com.example.solicitudes.repositories.SolicitudRepository;
+
+@Service
+public class SolicitudService {
+	@Autowired
+	private SolicitudRepository solicitudRepository;
+	
+	public List<Solicitud> listar(){
+		return solicitudRepository.listar();
+	}
+	public void crear(Solicitud solicitud) {
+		solicitudRepository.crear(solicitud);
+	}
+
+}
